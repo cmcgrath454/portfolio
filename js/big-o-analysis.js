a940211474398fd728e7cf0ec932fd03ded31649
Colin McGrath <cmcgrath454@gmail.com>
2023-03-04 15:15:16 -0600

    initial commit

---
+++ js/big-o-analysis.js
@@ -0,0 +1,41 @@
+function analyze(btn) {
+  btn.disabled = true;
+  fetch('https://cm7vsl37jpzpcr5l2pqzsjppje0onpic.lambda-url.us-east-2.on.aws/', {
+    method: 'POST',
+    body: document.getElementById("code").value,
+  })
+    .then((response) => response.json()
+      .then(body => processResponse(body, response.status))
+    );
+}
+
+function processResponse(body, status) {
+  const codeFeedback = document.getElementById('code-feedback');
+  switch (status) {
+    case 422:
+      let errors = body.message.match(/line: (\d*)/);
+      if (errors) {
+        const errorLocation = errors[1];
+        codeFeedback.innerHTML = '<span class="text-danger"> Invalid Java code detected at line ' + errorLocation + '</span>';
+      } else {
+        codeFeedback.innerText = '<span class="text-danger">Invalid Java code detected</span>';
+      }
+      break;
+    case 201:
+      const formattedResult = body.result.replace(/\^(\d*)/g, '<sup>$1</sup>').replaceAll('N', 'n');
+      document.getElementById('solution').insertAdjacentHTML('beforeend', 'Result: ' + formattedResult);
+      let inputCode = document.getElementById('code').value;
+      if (body.unsupported.length == 0) {
+        codeFeedback.innerHTML = inputCode;
+      } else {
+        body.unsupported.reverse().forEach((loc) => {
+          inputCode = inputCode.slice(0, loc.start) + '<span class="text-danger text-decoration-underline">' + inputCode.slice(loc.start, loc.end + 1) + '</span>' + inputCode.slice(loc.end + 1);
+        });
+      }
+      codeFeedback.innerHTML = inputCode;
+      break;
+    default:
+  }
+  codeFeedback.scrollIntoView();
+  document.getElementById('analyze-btn').disabled = false;
+}
